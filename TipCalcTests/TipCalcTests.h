472a86d0cd0b5415acb7e01657f98a938bf0388f
Veronica Schade <veganbunnie@gmail.com>
2012-01-27 01:33:01 -0500

    Initial Commit

---
+++ TipCalcTests/TipCalcTests.h
@@ -0,0 +1,13 @@
+//
+//  TipCalcTests.h
+//  TipCalcTests
+//
+//  Created by Veronica Schade on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TipCalcTests : SenTestCase
+
+@end
---
+++ TipCalcTests/TipCalcTests.m
@@ -0,0 +1,32 @@
+//
+//  TipCalcTests.m
+//  TipCalcTests
+//
+//  Created by Veronica Schade on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TipCalcTests.h"
+
+@implementation TipCalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TipCalcTests");
+}
+
+@end
