7e3321c5ab433d858a281b038a78525918022360
Veronica Schade <veganbunnie@gmail.com>
2012-03-12 00:22:02 -0400

    added TipCalc

--- TipCalc/ViewController.h
+++ TipCalc/ViewController.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UILabel *tipAmount; // to display the tip amount
+    IBOutlet UILabel *totalPay; // to display the total amount to pay
+    IBOutlet UITextField *totalBill; // to enter the total bill
+    IBOutlet UITextField *tipPercent; // to enter the tip percent you want to leave
+}
+
+@property(nonatomic, retain) IBOutlet UILabel *tipAmount;
+@property(nonatomic, retain) IBOutlet UILabel *totalPay;
+@property(nonatomic, retain) IBOutlet UITextField *totalBill;
+@property(nonatomic, retain) IBOutlet UITextField *tipPercent;
+
+-(IBAction)calculate; // the button to calculate the numbers
+-(IBAction)calculate:(id)sender; // the button to show the alert
+-(IBAction)reset; // the button to clear the text fields
+-(IBAction)hideKeyboard:(id)sender; // invisible button to hide keyboard
 
 @end
--- TipCalc/ViewController.m
+++ TipCalc/ViewController.m
@@ -10,6 +10,69 @@
 
 @implementation ViewController
 
+@synthesize tipAmount, totalPay, totalBill, tipPercent;
+
+-(IBAction) calculate; { // to calculate the numbers
+    double bill = [totalBill.text doubleValue];
+    double tip = [tipPercent.text doubleValue];
+    if (bill >= 0 && tip >= 0) { // if statement
+    double tipIs = (tip / 100) * bill; 
+    double billIs = (tipIs + bill);
+    NSString *resultTip = [[NSString alloc] initWithFormat: @"Tip Amount: $%.2f", tipIs];
+    NSString *resultPay = [[NSString alloc] initWithFormat: @"Total To Pay: $%.2f", billIs];
+    tipAmount.text = resultTip; // where the tip $ amount displays
+    totalPay.text = resultPay; // where the total to pay $ amount displays
+    }
+}
+
+-(IBAction) calculate:(id)sender { // the alert function
+    double bill = [totalBill.text doubleValue];
+    double tip = [tipPercent.text doubleValue];
+    if (bill < 0 || tip < 0) { // if statement
+    UIAlertView *alertDialog; 
+	alertDialog = [[UIAlertView alloc] 
+				   initWithTitle: @"Oh no!"
+				   message:@"Please enter a positive value for Total Bill and Tip Percent" 
+				   delegate:self 
+				   cancelButtonTitle:@"OK" 
+				   otherButtonTitles: nil]; 
+        [alertDialog show]; // the alert function could end here but continues to change the values 
+        NSString *resetBill = [[NSString alloc] initWithFormat: @"0.00", bill]; // changes bill to 0.00
+        NSString *resetTip = [[NSString alloc] initWithFormat: @"20", tip]; // changes tip to 20
+        totalBill.text = resetBill;
+        tipPercent.text = resetTip;
+	//[alertDialog release]; 
+
+    }
+}
+
+-(IBAction) reset; { // button to reset (clear) the amounts
+    double bill = [totalBill.text doubleValue];
+    double tip = [tipPercent.text doubleValue];
+    NSString *resetBill = [[NSString alloc] initWithFormat: @"", bill];
+    NSString *resetTip = [[NSString alloc] initWithFormat: @"", tip];
+    totalBill.text = resetBill;
+    tipPercent.text = resetTip;
+    double tipIs; //= (tip / 100) * bill; // just added this to clear the calculation results too
+    double billIs; //= (tipIs + bill);
+    NSString *resultTip = [[NSString alloc] initWithFormat: @"", tipIs];
+    NSString *resultPay = [[NSString alloc] initWithFormat: @"", billIs];
+    tipAmount.text = resultTip; // where the tip $ amount displays
+    totalPay.text = resultPay; // where the total to pay $ amount displays
+}
+
+-(IBAction) hideKeyboard:(id)sender {
+    [sender resignFirstResponder];
+    [totalBill resignFirstResponder]; 
+    [tipPercent resignFirstResponder];
+}
+
+
+
+
+
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
